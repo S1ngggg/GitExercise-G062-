a7294e4648993c7fce1b05037731e02056e28d2c
Kwong Jia Qi <jiaqikwong7@gmail.com>
2026-04-13 12:33:38 +0800

    update homepage

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
