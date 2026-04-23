7fd578ca274e04ac3e83199be728aea30feba2f1
Joshua Wong <sing7207@gmail.com>
2026-04-23 20:54:28 +0800

    Added some default categories and statuses, then made an item_form route that lets user create a form for their item

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template
+# import tools for handling form data and page navigation
+from flask import request, redirect, url_for
+from flask import Flask, render_template
 import sqlite3
 
-app = Flask(__name__,template_folder='templates',static_folder='static',static_url_path='/')
+app = Flask(__name__, template_folder='templates',
+            static_folder='static', static_url_path='/')
 
 
 # Creating the tables
@@ -37,11 +40,29 @@ def create_database():
                    category_id INTEGER NOT NULL,
                    status_id INTEGER NOT NULL,
                    price REAL NOT NULL,
-                   FOREIGN KEY(category_id) REFERENCES category(id)
+                   FOREIGN KEY(category_id) REFERENCES category(id),
                    FOREIGN KEY(status_id) REFERENCES status(id)
                    )
                    """)
-    #create user table if does not exist yet
+
+    # Adding default categories
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO category (name) VALUES ('Electronics')")
+    cursor.execute("INSERT OR IGNORE INTO category (name) VALUES ('Books')")
+    cursor.execute(
+        "INSERT OR IGNORE INTO category (name) VALUES ('Furniture')")
+    cursor.execute(
+        "INSERT OR IGNORE INTO category (name) VALUES ('Second-hand')")
+   # Adding default and possibly permanent status conditions
+
+    cursor.execute("INSERT OR IGNORE INTO status(condition) VALUES ('Sold')")
+    cursor.execute(
+        "INSERT OR IGNORE INTO status(condition) VALUES ('Reserved')")
+    cursor.execute(
+        "INSERT OR IGNORE INTO status(condition) VALUES ('Available')")
+
+    # create user table if does not exist yet
     cursor.execute("""
                    
                 CREATE TABLE IF NOT EXISTS user(
@@ -53,29 +74,9 @@ def create_database():
                 role TEXT NOT NULL
                 )
 
-<<<<<<< HEAD
-# Adding default categories
-
-    cursor.execute(
-        "INSERT OR IGNORE INTO category (name) VALUES ('Electronics')")
-    cursor.execute("INSERT OR IGNORE INTO category (name) VALUES ('Books')")
-    cursor.execute(
-        "INSERT OR IGNORE INTO category (name) VALUES ('Furniture')")
-
-# Adding default and possibly permanent status conditions
-
-    cursor.execute("INSERT OR IGNORE INTO status(condition) VALUES ('Sold')")
-    cursor.execute(
-        "INSERT OR IGNORE INTO status(condition) VALUES ('Reserved')")
-    cursor.execute(
-        "INSERT OR IGNORE INTO status(condition) VALUES ('Available')")
-
-    connect.commit()
-=======
                 """)
 
-    connect.commit()#save change to the database
->>>>>>> a7b353f01de87afcdc9859a39d7844bd5af4b033
+    connect.commit()  # save change to the database
     connect.close()
 
 
@@ -83,12 +84,12 @@ def create_database():
 def home():
     return render_template("index.html")
 
-from flask import request, redirect, url_for #import tools for handling form data and page navigation
 
-@app.route("/register", methods=['GET','POST'])#accept displaying form and processing form
+# accept displaying form and processing form
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':#if user submit form
-        email = request.form['email']#data from frontend
+    if request.method == 'POST':  # if user submit form
+        email = request.form['email']  # data from frontend
         username = request.form['username']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
@@ -98,30 +99,49 @@ def register():
         if password != confirm_password:
             return "Password not match"
 
-
-        conn = sqlite3.connect('database.db')#connect to sqlite database
+        conn = sqlite3.connect('database.db')  # connect to sqlite database
         cursor = conn.cursor()
 
         cursor.execute("""
             INSERT INTO user (email, username, password, gender, role)
             VALUES(?, ?, ?, ?, ?)
-            """,(email, username, password, gender, role)) #insert user data using ? 
-        
+            """, (email, username, password, gender, role))  # insert user data using ?
+
         conn.commit()
         conn.close()
 
-        return redirect(url_for('login'))#after form submitted jump to login page
-    
+        # after form submitted jump to login page
+        return redirect(url_for('login'))
+
     return render_template("register.html")
 
+
 @app.route("/login")
-def login() :
+def login():
     return render_template("login.html")
 
 
 @app.route("/home")
 def home_page():
     return render_template("home.html")
+
+
+@app.route("/item_form")
+def item_form():
+    connect = sqlite3.connect("database.db")
+    cursor = connect.cursor()
+
+    # getting all the categories
+    cursor.execute("SELECT * FROM category")
+    categories_list = cursor.fetchall()
+
+    # getting all statuses
+    cursor.execute("SELECT * FROM status")
+    status_list = cursor.fetchall()
+
+    connect.close()
+
+    return render_template("item_form.html", categories=categories_list, status=status_list)
 
 
 if __name__ == "__main__":
