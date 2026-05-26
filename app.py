1e545164374842763eabbbf51b7853a4d8caf3f0
Joshua Wong <sing7207@gmail.com>
2026-05-26 17:11:03 +0800

    merged with main

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # import tools for handling form data and page navigation
 from flask import request, redirect, url_for, flash, session
 from flask import Flask, render_template
-from flask_mail import Mail, Message    
+from flask_mail import Mail, Message
 import random
 from datetime import datetime, timedelta
 import sqlite3
@@ -10,15 +10,19 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__, template_folder='templates',
             static_folder='static', static_url_path='/')
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com' #configure mail server, here using gmail
-app.config['MAIL_PORT'] = 587   #configure mail port, 587 is commonly used for secure email submission
-app.config['MAIL_USE_TLS'] = True #enable secure email encryption
-app.config['MAIL_USERNAME'] = 'valorantsing2007@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'idzw zprj hqak xypr' #use app password for security, generate from google account setting
+# configure mail server, here using gmail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# configure mail port, 587 is commonly used for secure email submission
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True  # enable secure email encryption
+app.config['MAIL_USERNAME'] = 'valorantsing2007@gmail.com'
+# use app password for security, generate from google account setting
+app.config['MAIL_PASSWORD'] = 'idzw zprj hqak xypr'
 
-mail = Mail(app) #initialize flask-mail, connect to flaskapp
+mail = Mail(app)  # initialize flask-mail, connect to flaskapp
 
 # Creating the tables
+
 
 def create_database():
     connect = sqlite3.connect("database.db")
@@ -191,7 +195,8 @@ def login():
         cursor = conn.cursor()
 
         # serching user by email
-        cursor.execute("""SELECT id, email, username, password FROM user WHERE email =?""", (email,))
+        cursor.execute(
+            """SELECT id, email, username, password FROM user WHERE email =?""", (email,))
         user = cursor.fetchone()
         conn.close()
 
@@ -207,7 +212,7 @@ def login():
         if check_password_hash(stored_password, password):
             # create session, store user identity in session
             # remember which user is logged in by storing user id in session
-            session['user_id'] = user_id 
+            session['user_id'] = user_id
             session['email'] = user_email
             session['username'] = username
 
@@ -240,13 +245,18 @@ def forgot_password():
         conn.close()
 
         if user:
-            otp = str(random.randint(100000, 999999)) #generate random 6 digit otp
-            session['otp'] = otp #store otp in session to compare later
-            session['otp_expiry'] = (datetime.now() + timedelta(minutes=10)).strftime('%Y-%m-%d %H:%M:%S') #set otp expiry time to 10 minutes
+            # generate random 6 digit otp
+            otp = str(random.randint(100000, 999999))
+            session['otp'] = otp  # store otp in session to compare later
+            session['otp_expiry'] = (datetime.now() + timedelta(minutes=10)).strftime(
+                '%Y-%m-%d %H:%M:%S')  # set otp expiry time to 10 minutes
 
-            session['reset_email'] = email #store email in session to identify which user is resetting password
-            msg = Message("Password Reset OTP", sender=app.config['MAIL_USERNAME'], recipients=[email]) #create email message
-            username = user[2] #get username from database result, index 2 is username column
+            # store email in session to identify which user is resetting password
+            session['reset_email'] = email
+            msg = Message("Password Reset OTP", sender=app.config['MAIL_USERNAME'], recipients=[
+                          email])  # create email message
+            # get username from database result, index 2 is username column
+            username = user[2]
             msg.html = f"""
 <html>
     <body>
@@ -261,10 +271,10 @@ def forgot_password():
         <p>Thank you.</p>
     </body>
 </html>"""
-            mail.send(msg) 
+            mail.send(msg)
 
             flash("OTP sent to your email. Please check your inbox.")
-            return redirect(url_for('check_otp')) #redirect to check otp page 
+            return redirect(url_for('check_otp'))  # redirect to check otp page
 
         else:
             flash("Email not found. Please enter a valid email.")
@@ -272,36 +282,36 @@ def forgot_password():
     return render_template("forgot_password.html")
 
 
-@app.route("/check-otp", methods=['GET','POST'])
+@app.route("/check-otp", methods=['GET', 'POST'])
 def check_otp():
-    if request.method == 'POST' :
+    if request.method == 'POST':
 
         user_otp = request.form['otp']
         stored_otp = session.get('otp')
         expiry_time = session.get('otp_expiry')
 
-        #session expired or no otp in session
+        # session expired or no otp in session
         if not stored_otp or not expiry_time:
             flash("No OTP found. Please request a new OTP.")
             return redirect(url_for('forgot_password'))
-        
-        #otp expired
+
+        # otp expired
         if datetime.now() > datetime.strptime(expiry_time, '%Y-%m-%d %H:%M:%S'):
             flash("OTP has expired. Please request a new OTP.")
             return redirect(url_for('forgot_password'))
-        #otp correct
-        if user_otp ==stored_otp:
-            #clear otp after success verify
+        # otp correct
+        if user_otp == stored_otp:
+            # clear otp after success verify
             session.pop('otp', None)
             session.pop('otp_expiry', None)
             flash("OTP verified. Please reset your password.")
-            #redirect to reset password page and pass email to identify which user is resetting password
-            return redirect(url_for('reset_password',email=session.get('reset_email'))) 
-        
+            # redirect to reset password page and pass email to identify which user is resetting password
+            return redirect(url_for('reset_password', email=session.get('reset_email')))
+
         else:
             flash("Invalid OTP. Please try again.")
             return redirect(url_for('check_otp'))
-            
+
     return render_template("check-otp.html")
 
 
@@ -356,8 +366,9 @@ def home_page():
 
 @app.route("/profile")
 def profile():
-    username = session.get('username', 'Your')  # get username from session, default to 'Your' if not found
-    return render_template("user_profile.html", username=username) 
+    # get username from session, default to 'Your' if not found
+    username = session.get('username', 'Your')
+    return render_template("user_profile.html", username=username)
 
 
 @app.route("/marketplace", methods=['GET'])
@@ -593,4 +604,4 @@ def item_saved():
 
 if __name__ == "__main__":
     create_database()
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
